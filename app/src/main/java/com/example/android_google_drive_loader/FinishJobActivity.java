e947f0f2ecf7b02223ae3994858ceb70fd9799a9
Egor Isichenko <eisichenko@mail.ru>
2021-08-24 22:59:06 +0300

    fixed bug with theme menu and changed progress bar color

--- app/src/main/java/com/example/android_google_drive_loader/FinishJobActivity.java
+++ app/src/main/java/com/example/android_google_drive_loader/FinishJobActivity.java
@@ -44,80 +44,82 @@ public class FinishJobActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        if (MainActivity.operationType == OperationType.PUSH) {
-            MainActivity.driveHelper.push(this, MainActivity.pickedDir, MainActivity.driveFolderName)
-                    .addOnSuccessListener(isTestPassed -> {
-                        if (isTestPassed) {
-                            MainActivity.driveHelper.showToast("SUCCESS");
-                        }
-                        else {
-                            MainActivity.driveHelper.showToast("FAIL (folders are not equal)");
-                        }
+        if (savedInstanceState == null) {
+            if (MainActivity.operationType == OperationType.PUSH) {
+                MainActivity.driveHelper.push(this, MainActivity.pickedDir, MainActivity.driveFolderName)
+                        .addOnSuccessListener(isTestPassed -> {
+                            if (isTestPassed) {
+                                MainActivity.driveHelper.showToast("SUCCESS");
+                            }
+                            else {
+                                MainActivity.driveHelper.showToast("FAIL (folders are not equal)");
+                            }
 
-                        MainActivity.stopwatchState = StopwatchState.STOPPED;
-                    })
-                    .addOnFailureListener(e -> {
-                        MainActivity.driveHelper.showToast(e.getMessage());
-                        System.out.println(e.getMessage());
-                        e.printStackTrace();
+                            MainActivity.stopwatchState = StopwatchState.STOPPED;
+                        })
+                        .addOnFailureListener(e -> {
+                            MainActivity.driveHelper.showToast(e.getMessage());
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
 
-                        MainActivity.stopwatchState = StopwatchState.STOPPED;
-                        backButton.setVisibility(View.VISIBLE);
-                    });
-        }
-        else if (MainActivity.operationType == OperationType.PULL) {
-            MainActivity.driveHelper.pull(this, MainActivity.pickedDir, MainActivity.driveFolderName)
-                    .addOnSuccessListener(isTestPassed -> {
-                        if (isTestPassed) {
-                            MainActivity.driveHelper.showToast("SUCCESS");
-                        }
-                        else {
-                            MainActivity.driveHelper.showToast("FAIL (folders are not equal)");
-                        }
-
-                        MainActivity.stopwatchState = StopwatchState.STOPPED;
-                    })
-                    .addOnFailureListener(e -> {
-                        MainActivity.driveHelper.showToast(e.getMessage());
-                        System.out.println(e.getMessage());
-                        e.printStackTrace();
-
-                        MainActivity.stopwatchState = StopwatchState.STOPPED;
-                        backButton.setVisibility(View.VISIBLE);
-                    });
-        }
-        else {
-            MainActivity.driveHelper.showToast("ERROR: Unknown operation");
-            MainActivity.stopwatchState = StopwatchState.STOPPED;
-            backButton.setVisibility(View.VISIBLE);
-        }
-
-        timeTextView = findViewById(R.id.timeTextView);
-        final Handler handler = new Handler();
-
-        MainActivity.stopwatchState = StopwatchState.RUNNING;
-
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (MainActivity.stopwatchState == StopwatchState.RUNNING) {
-                    currentTimeInSeconds++;
-
-                    Integer minutes = currentTimeInSeconds / 60;
-                    Integer seconds = currentTimeInSeconds % 60;
-
-                    if (minutes > 0) {
-                        timeTextView.setText(getResources().getString(R.string.time_text_view_value)
-                                + " " + minutes + " m " + seconds.toString() + " s");
-                    }
-                    else {
-                        timeTextView.setText(getResources().getString(R.string.time_text_view_value)
-                                + " " + seconds.toString() + " s");
-                    }
-
-                    handler.postDelayed(this, 1000);
-                }
+                            MainActivity.stopwatchState = StopwatchState.STOPPED;
+                            backButton.setVisibility(View.VISIBLE);
+                        });
             }
-        });
+            else if (MainActivity.operationType == OperationType.PULL) {
+                MainActivity.driveHelper.pull(this, MainActivity.pickedDir, MainActivity.driveFolderName)
+                        .addOnSuccessListener(isTestPassed -> {
+                            if (isTestPassed) {
+                                MainActivity.driveHelper.showToast("SUCCESS");
+                            }
+                            else {
+                                MainActivity.driveHelper.showToast("FAIL (folders are not equal)");
+                            }
+
+                            MainActivity.stopwatchState = StopwatchState.STOPPED;
+                        })
+                        .addOnFailureListener(e -> {
+                            MainActivity.driveHelper.showToast(e.getMessage());
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
+
+                            MainActivity.stopwatchState = StopwatchState.STOPPED;
+                            backButton.setVisibility(View.VISIBLE);
+                        });
+            }
+            else {
+                MainActivity.driveHelper.showToast("ERROR: Unknown operation");
+                MainActivity.stopwatchState = StopwatchState.STOPPED;
+                backButton.setVisibility(View.VISIBLE);
+            }
+
+            timeTextView = findViewById(R.id.timeTextView);
+            final Handler handler = new Handler();
+
+            MainActivity.stopwatchState = StopwatchState.RUNNING;
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (MainActivity.stopwatchState == StopwatchState.RUNNING) {
+                        currentTimeInSeconds++;
+
+                        Integer minutes = currentTimeInSeconds / 60;
+                        Integer seconds = currentTimeInSeconds % 60;
+
+                        if (minutes > 0) {
+                            timeTextView.setText(getResources().getString(R.string.time_text_view_value)
+                                    + " " + minutes + " m " + seconds.toString() + " s");
+                        }
+                        else {
+                            timeTextView.setText(getResources().getString(R.string.time_text_view_value)
+                                    + " " + seconds.toString() + " s");
+                        }
+
+                        handler.postDelayed(this, 1000);
+                    }
+                }
+            });
+        }
     }
 }
